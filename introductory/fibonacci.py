a2f3cf6e2e6f4401032260aacb16d4cc36a4aab7
Aditya Raman <32497523+ramanaditya@users.noreply.github.com>
2018-10-06 18:25:38 +0530

    Update fibonacci.py

--- introductory/fibonacci.py
+++ introductory/fibonacci.py
@@ -2,12 +2,23 @@
 
 import sys
 
+# This algorithm is quite fast but it uses too much storage to store the data 
 def calc_fib(n):
 	fib_list = list(range(0,n+1))
 	for i in range(2,n+1):
 		fib_list[i] = fib_list[i-1] + fib_list[i-2]
 	return fib_list[n]
 
+# This algorithm is quite fast as well as doesn't utilises too muc h spaces
+# The running time of this algorithm is O(n)
+def fib(n):
+	ifn<=1:
+		return n
+	previous , current = 0, 1
+	for _ in range(n − 1):
+		new_current = previous + current
+		previous , current = current , new_current
+	return current
 
 a = int(input())
 print(calc_fib(a))
